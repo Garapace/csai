e094d6005d79ebab11fd8bae2b3c5295045d3b54
Garapace <starkov-2003@list.ru>
2025-10-15 17:14:15 +0500

    lab 1, task 4

---
+++ lab1/lab04/lib.py
@@ -0,0 +1,25 @@
+""" Создайте файл lib.py в директории проекта с функцией,
+которая принимает на вход N списков и возвращает количество одинаковых элементов в них """
+
+
+def common_elements_count(*lists):
+    if not lists:
+        return 0
+
+    common_list = set(lists[0])     # преобразуем первый список в множество
+    for LIST in lists[1:]:          # для каждого следующего списка
+        common_list &= set(LIST)    # находим пересечение (общие элементы)
+ 
+    return len(common_list)
+
+
+def main():
+    array1 = [1, 2, 3, 90, 45, 6, 12, 52]
+    array2 = [1, 123, 3, 90, 132, 6, 52]
+    array3 = [1, 123, 3, 90, 132, 6, 52, 123, 4, 345]
+    print(f"кол-во одинаковых элементов в списках - {common_elements_count(array1, array2, array3)}")
+
+
+if __name__ == "__main__":
+    main()
+/1
