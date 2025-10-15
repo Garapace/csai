260a8e4a92d376cf818b9a9dd45687b3fdd51e70
Garapace <starkov-2003@list.ru>
2025-10-15 17:43:50 +0500

    lab 3

---
+++ lab3/test_lib.py
@@ -0,0 +1,6 @@
+from lib import shaker_sort
+
+def test_shaker_sort_positive():
+    assert shaker_sort([5, 3, 8, 4, 2, 7, 1, 10]) == sorted([5, 3, 8, 4, 2, 7, 1, 10])
+def test_shaker_sort_negative():
+    assert not shaker_sort([1, 2, 3, 4, 5]) == [1, 2, 4, 3, 5]
